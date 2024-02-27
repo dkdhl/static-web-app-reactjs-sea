c0512a3a8f01f405463f2d5c939ae46e08dfe378
dkdhl <134286412+dkdhl@users.noreply.github.com>
2024-02-27 14:26:38 +0800

    Update App.js
    add link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 function App() {
   const value = 'World - from SEA';
-  return <div>Hello {value}</div>;
+  return <div>
+    Hello {value}
+    <a href="https://http-trigger-poc4its.azurewebsites.net/api/HttpTriggerJava?code=R4KN_ks3Y_xTOYvXFLTNPw0XomrQEUxFCM5yuRtZy5MXAzFuR9kuDw==&name=dickson">Click me</a>
+
+  </div>;
 }
 
 export default App;
