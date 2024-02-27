5eff6e9f81c8927eb2b4b6b75ca4933799cec8ba
dkdhl <134286412+dkdhl@users.noreply.github.com>
2024-02-27 14:49:59 +0800

    Update App.js
    add br

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   const value = 'World - from SEA';
   return <div>
     Hello {value}
-    <a href="https://http-trigger-poc4its.azurewebsites.net/api/HttpTriggerJava?code=R4KN_ks3Y_xTOYvXFLTNPw0XomrQEUxFCM5yuRtZy5MXAzFuR9kuDw==&name=dickson">Click me</a>
+  <br/>
+  
+    <a href="https://http-trigger-poc4its.azurewebsites.net/api/HttpTriggerJava?code=R4KN_ks3Y_xTOYvXFLTNPw0XomrQEUxFCM5yuRtZy5MXAzFuR9kuDw==&name=dickson">Click me to call Azure Function</a>
 
   </div>;
 }
