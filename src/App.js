1dcedbd490ae622360a1b110c26fcf5d6ab18c4d
dkdhl <134286412+dkdhl@users.noreply.github.com>
2024-02-28 13:45:21 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import React from 'react';
 function App() {
   const value = 'World - from SEA';
   return <div>
-    Hello {value}
-  <br/>
-  
+    Hello {value}  
     <a href="https://http-trigger-poc4its.azurewebsites.net/api/HttpTriggerJava?code=R4KN_ks3Y_xTOYvXFLTNPw0XomrQEUxFCM5yuRtZy5MXAzFuR9kuDw==&name=dickson">Click me to call Azure Function</a>
-
   </div>;
 }
 
